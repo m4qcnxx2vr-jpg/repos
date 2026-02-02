522372cb8ba52d4db79cb9748b7b127a021a6d9e
Nikolajhojen <m4qcnxx2vr@privaterelay.appleid.com>
2026-02-02 11:58:08 +0100

    Week 2, hello from hello, gamma, tgamma

--- Week_2/main.cc
+++ Week_2/main.cc
@@ -8,5 +8,9 @@ int main(){
  double y = sfuns::fgamma(x);
  std::cout << "fgamma(1)="<< y<< "\n";
  std::printf("fgamma(1)=%g\n",y);
+ for(double x=1;x<=9;x+=1){
+    std::cout <<"fgamma(" << x <<") =" << sfuns:: fgamma(x)
+        << " tgamma("<< x <<")="<< std::tgamma(x) << "\n";
+    }
  return 0;
 }
